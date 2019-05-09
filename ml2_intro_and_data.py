6f21f986159ba811d91995a957f1814e790566c4
--help <roger.lren@gmail.com>
2019-05-09 15:14:30 +0800

    sklearning code snippet

---
+++ ml2_intro_and_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+df = quandl.get('WIKI/GOOGL')
+#print(df.head)
+
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
+#print(df.head)
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-r = requests.get("https://www.gmail.com")
-
-print(r.status_code)
-print(r.ok)
